7cc3131b50be5bdd179c5c07d2cec8acc862ca21
Manas Ranjan Bera <manasbera484@gmail.com>
2025-07-25 12:52:45 +0530

    Conditional Statement

---
+++ JavaScript/Class 3/script.js
@@ -0,0 +1,48 @@
+//! Conditional Statements
+//? if statement
+let age = 10
+// if (age > 18) {
+//     console.log("You can vote");
+// }
+
+//? if-else
+// if (age > 18) {
+//     console.log("You can vote");
+// } else {
+//     console.log("You can't vote");
+// }
+
+//? if-elif
+let time = 20
+// if (time > 0 && time < 11) {
+//     console.log("Good Morning");
+// } else if (time >= 11 && time < 17) {
+//     console.log("Good Afternoon");
+// } else if (time >= 17 && time < 20) {
+//     console.log("Good evening");
+// } else if (time >= 20 && time <= 24) {
+//     console.log("Good night");
+// } else {
+//     console.log("Invalid Time");
+// }
+
+//? switch case
+let val = 8
+// switch (val) {
+//     case 1:
+//         console.log("Case 1");
+//         break
+//     case 2:
+//         console.log("Case 2");
+//         break
+//     case 3:
+//         console.log("Case 3");
+//         break
+//     case 4:
+//         console.log("Case 4");
+//         break
+//     default:
+//         console.log("Default Case");
+//         break
+// }
+
