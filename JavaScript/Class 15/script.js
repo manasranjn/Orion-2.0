849607a72cfaa923cc830ec0f258e057e2fdc6f2
Manas Ranjan Bera <manasbera484@gmail.com>
2025-08-16 13:18:26 +0530

    Project 1

--- JavaScript/Class 15/script.js
+++ JavaScript/Class 15/script.js
@@ -21,4 +21,39 @@ const getData = async () => {
     const data = await response.json()
     console.log(data);
 }
-getData()
+// getData()
+
+const box = document.getElementById("box")
+
+fetch("https://api.escuelajs.co/api/v1/products")
+    .then((res) => res.json())
+    .then((result) => {
+        console.log(result);
+        result.map((product) => {
+            // console.log("Product is", product);
+
+            const productName = document.createElement("h1");
+            const description = document.createElement("p")
+            const price = document.createElement("p")
+            const image = document.createElement("img")
+
+            productName.innerText = product.title
+            description.textContent = product.description
+            price.innerText = "Price: $" + product.price
+
+            // image.setAttribute("src", product.images[0])
+            product.images.map((i) => {
+                console.log(i);
+                image.setAttribute("src", i)
+            })
+
+            box.append(productName, description, price, image)
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+        const errorMsg = document.createElement("p")
+        errorMsg.innerText = "Failed to fetch Data"
+
+        box.append(errorMsg)
+    });
